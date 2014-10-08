1d09b8e5465d494c71f574bf3e22546801711918
Leo Correa <lcorr005@gmail.com>
2014-10-08 10:54:20 -0400

    Upgrade go version

--- go/go_vars.zsh
+++ go/go_vars.zsh
@@ -1,4 +1,4 @@
-export GOVERSION="1.3.1"
+export GOVERSION="1.3.3"
 export GOPATH=$(brew --prefix)/Cellar/go/$GOVERSION
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:/usr/local/opt/go/libexec/bin

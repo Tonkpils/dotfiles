7f84c862b231e1c1ef5d4e23382faf4b13f606ff
Leo Correa <lcorr005@gmail.com>
2015-08-24 01:23:25 -0400

    Fix omzsh theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -33,3 +33,5 @@ else
   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+mkdir -p "$HOME/.oh-my-zsh/custom/themes"
+ln -s "$WORKDIR/oh-my-zsh/custom/tonkpils.zsh-theme" "$HOME/.oh-my-zsh/custom/themes"
